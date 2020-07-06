6448fde863ba4e2fcd7894e4fcb913eccbdecabe
Matthew Lacayo <mattlacayo@berkeley.edu>
2020-07-06 14:22:30 -0700

    Finished implementing login

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -2,14 +2,22 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.parstagram.databinding.ActivityLoginBinding;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
 
     private ActivityLoginBinding binding;
 
@@ -28,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(rootView);
 
         bindElements();
+        setupButtons();
     }
 
     private void bindElements() {
@@ -36,4 +45,60 @@ public class LoginActivity extends AppCompatActivity {
         btLogin = binding.btLogin;
         btSignup = binding.btSignup;
     }
+
+    private void setupButtons() {
+        setupLoginButton();
+        setupSignupButton();
+    }
+
+    private void setupSignupButton() {
+        btSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleSignup();
+            }
+        });
+    }
+
+    private void handleSignup() {
+    }
+
+    private void setupLoginButton() {
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleLogin();
+            }
+        });
+    }
+
+    private void handleLogin() {
+        Log.i(TAG, "Performing login");
+
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with sign-in.", e);
+                    Toast.makeText(LoginActivity.this, "Login failed! Please make sure credentials " +
+                            "are correct and try again.", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    Log.i(TAG, "Sign in successful.");
+                    gotoMainActivity();
+                }
+
+            }
+        });
+    }
+
+    private void gotoMainActivity() {
+        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+
 }
